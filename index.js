7b73946969dc9362720bafac1f42f2844664558b
kyledodd <kyle.e.dodd@outlook.com>
2021-07-09 10:53:08 -0400

    reverting to pre-rotation.json for now

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static('images'));
 const config = require("./config.json");
-const fs = require('fs');
 
 // init framework
 framework = new framework(config);
@@ -48,9 +47,6 @@ framework.on('spawn', (bot, id, actorId) => {
 
 // Process incoming messages
 let responded = false;
-let rotation_data = fs.readFileSync("rotation.json", "utf8");
-var rotation = JSON.parse(rotation_data);
-/*
 let rotation = [
                 'kyle.stephens1@metlife.com|Kyle Stephens',
                 'aj.langlois@metlife.com|AJ Langlois',
@@ -58,7 +54,6 @@ let rotation = [
                 'vvobbilichetty@metlife.com|Vinay Vobbilichetty',
                 'cedric.smith@metlife.com|Cedric Smith'
               ];
-*/
 
 /* On mention with command
 ex User enters @botname help, the bot will write back in markdown
@@ -69,9 +64,8 @@ framework.hears('help', function (bot) {
   bot.say("markdown", 'These are the commands I can respond to:', '\n\n ' +
       '**who**  (get the name of the current METCIRT on-call person) \n' +
       '**rotation**  (get the rotation details) \n' +
-      '**set**  (set a different on-call outside of rotation) \n' +
-      '**dev** (get developer details) \n' +
       '**responsibilities**  (get the on-call responsibilities) \n' +
+      '**dev** (get the developer details) \n' +
       '**help** (what you are reading now)')
     .catch((e) => console.error(`Problem in help handler: ${e.message}`));
 });
@@ -82,7 +76,7 @@ ex User enters @botname who, the bot will write back in markdown
 framework.hears('who', function (bot) {
   console.log("who command received");
   responded = true;
-  bot.say("markdown", `The METCIRT on-call person is <@personEmail:${Object.values(rotation)[0]}>`);
+  bot.say("markdown", `The METCIRT on-call person is <@personEmail:${rotation[0]}>`);
 });
 
 /* On mention with command, using other trigger data, can use lite Markdown formatting
@@ -91,7 +85,7 @@ ex User enters @botname 'info' phrase, the bot will provide personal details
 framework.hears('dev', function (bot) {
   console.log("dev command received");
   responded = true;
-  bot.say("markdown", 'This bot is maintained by <@personEmail:vinay.vobbilichetty@metlife.com|Vinay Vobbilichetty> from METCIRT. Reach out to him for feedback or feature requests');
+  bot.say("markdown", 'This bot is maintained by <@personEmail:vvobbilichetty@metlife.com|Vinay Vobbilichetty> from METCIRT. Reach out to him for feedback or feature requests');
 });
 
 /* On mention with command, using other trigger data, can use lite Markdown formatting
@@ -100,10 +94,7 @@ ex User enters @botname 'info' phrase, the bot will provide personal details
 framework.hears('rotation', function (bot) {
   console.log("rotation command received");
   responded = true;
-  //
-  // I'm not sure if this will work. I think it might, but it's hard to test.
-  //
-  let rotation_display = Object.values(rotation).map(person => {
+  let rotation_display = rotation.map(person => {
     return '<@personEmail:' + person + '>';
   })
   let message = 'Here are the rotation details \n' + rotation_display.join('\n');
@@ -116,25 +107,11 @@ ex User enters @botname 'details' phrase, bot will provide the details regarding
 framework.hears('responsibilities', function (bot) {
     console.log("responsibilities command received");
     responded = true;
-    link = 'https://my.metlife.com/sites/CyberSecurity-Tier3/_layouts/15/Doc.aspx?sourcedoc={2ef52a56-8635-4834-b90a-5eac426c5007}&action=edit&wd=target%28Team%20Info.one%7C748ea7db-c469-6246-ba69-1ebd186ca725%2FOn-Call%20Policy%7Ca5e2b24f-a141-6c48-a8b2-c1cf451d5b62%2F%29';
     bot.say("markdown", 'T3 On-Call responsibilities are as follows:\n' +
         'Shift Details: The on-call rotation starts at 9AM EST on Mondays and lasts until same time the following Monday.\n' +
         'Responsibilities: Maintain 24/7 availability, watch for and ack critical escalations, may run point on high priority escalations.\n' +
-        `Further: Monitor dashboards and lead weekly regional calls. Please see [OneNote](${link}) for further instruction.`);
+        'Further: Monitor dashboards and lead weekly regional calls. Please see OneNote for further instruction.');
 });
-
-/* On mention with command, set @Mention
-ex User enters @botname set @Mention phrase, bot will set the current on-call to @Mention
-*/
-framework.hears(/^.*set.*/i, function (bot)) {
-    console.log("set command received");
-    responded = true;
-    let trigger_text = `${trigger.text}`;
-    let name_array = trigger_text.split(" ");
-    let name = rotation(name_array[2]);
-    bot.say(`The On-Call is set to: <@personEmail:${name}`).catch((e) => console.error(`Problem in help handler: ${e.message}`));
-
-}
 
 /* On mention with unexpected bot command
    It's a good practice to gracefully handle unexpected input
